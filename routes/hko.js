d5e51ca4550188b978e7070f28fee7f2119a84f5
Jamie Wilde <jamie@dimsumlab.com>
2014-07-16 17:44:58 +0800

    setup response JSON, with some simple error checking

--- routes/hko.js
+++ routes/hko.js
@@ -1,4 +1,37 @@
 var request = require('request');
+var YQL_uri = "http://query.yahooapis.com/v1/public/yql/jamiewildehk/hko_warn_ts?format=json&date="
 exports.getTSW = function(req, res) {
-  res.send("Hello World");
+  request({ uri: YQL_uri + req.params.date, 
+           json: true}, 
+  function(err, response, body) {
+    if (err) { return res.send({ error : err}); }
+    if (body.query.results) {
+      var values = body.query.results.strong;
+      var results = {
+          warnings : values.length/4,
+          totalDuration : 0,
+          details : []
+      };
+      for(n=0;n<values.length;n+=4) {
+        var startDate = new Date(Date.parse(values[n].toString() 
+                                            + " " 
+                                            + values[n+1].toString()
+                                           )
+                                );
+        var endDate = new   Date(Date.parse(values[n+2].toString() 
+                                            + " " 
+                                            + values[n+3].toString()
+                                           )
+                                );
+        results.totalDuration += (endDate - startDate) / 60000;
+        results.details.push({
+          from : startDate,
+          to: endDate
+        });
+      }
+      return res.send({ warning : results});
+    } else {
+      return res.send({ warning : null});
+    }
+  });
 };
